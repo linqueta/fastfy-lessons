8bd74950e7501700967028f25e0f3352b1bef6ab
linqueta <lincolnrodrs@gmail.com>
2020-02-08 13:49:49 -0300

    set decorate reply

--- nice_plugin/my-plugin.js
+++ nice_plugin/my-plugin.js
@@ -4,4 +4,13 @@ module.exports = async function (fastify, options) {
   })
 
   fastify.decorate('util', (a, b) => a + b)
+
+  fastify.decorateReply('html', function (payload) {
+    this.type('text/html') // This is the 'Reply' object
+    this.send(`It's the confuse payload: ${payload}`)
+  })
+
+  fastify.get('/html', (request, reply) => {
+    reply.html('oioioi')
+  })
 }
