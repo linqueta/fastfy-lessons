78ca30820e0970616fd71137f9e5b390c3ab7285
linqueta <lincolnrodrs@gmail.com>
2020-02-08 13:18:56 -0300

    add util decorator

--- nice_plugin/my-plugin.js
+++ nice_plugin/my-plugin.js
@@ -1,5 +1,7 @@
 module.exports = async function (fastify, options) {
   fastify.get('/plugin', async (request, reply) => {
-    return { hello: 'world, big world' }
+    return { hello: fastify.util('world', ', big world wih decorator') }
   })
+
+  fastify.decorate('util', (a, b) => a + b)
 }
